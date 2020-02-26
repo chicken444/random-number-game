4cf9706e5a57735f25fc69fdffac19d86c1f9b39
chicken444 <61481878+chicken444@users.noreply.github.com>
2020-02-25 16:59:19 -0800

    Update random#.py

--- random#.py
+++ random#.py
@@ -1,5 +1,4 @@
 import random
-
 number = random.randint(1,100)
 number_of_guesses = 0
 win = 0
@@ -9,29 +8,24 @@ print(number)
 while number_of_guesses < 5 and win == 0:
     ans = input("guess a number?")
     if ans == '<':
-        if ans == '<':
             greater = input('greater than?')
             greater = int(greater)
             if greater < number:
                 print('yes')
+                number_of_guesses += 1
             else:
                 print('no')
-        if ans == '>':
-            less = input('less than?')
-            less = int(less)
-            if less > number:
-                print('yes')
-            else:
-                print('no')
+                number_of_guesses += 1
     elif ans == '>':
-        if ans == '>':
             less = input('less than?')
             less = int(less)
             if less > number:
                 print('yes')
+                number_of_guesses += 1
             else:
                 print('no')
-    if int(ans) == number:
+                number_of_guesses += 1
+    elif int(ans) == number:
         print('correct')
         win = 1
     else:
@@ -41,3 +35,4 @@ if win == 0:
     print('you lose.')
 else:
     print('you win!')
+            
