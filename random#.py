86da6bd50c496655d0ce4e846265b66502b5b136
chicken444 <61481878+chicken444@users.noreply.github.com>
2020-02-25 16:52:52 -0800

    Update random#.py

--- random#.py
+++ random#.py
@@ -7,32 +7,37 @@ greater = 'N.A'
 less = 'N.A'
 print(number)
 while number_of_guesses < 5 and win == 0:
-    ans = input("guess?")
+    ans = input("guess a number?")
+    if ans == '<':
+        if ans == '<':
+            greater = input('greater than?')
+            greater = int(greater)
+            if greater < number:
+                print('yes')
+            else:
+                print('no')
+        if ans == '>':
+            less = input('less than?')
+            less = int(less)
+            if less > number:
+                print('yes')
+            else:
+                print('no')
+    elif ans == '>':
+        if ans == '>':
+            less = input('less than?')
+            less = int(less)
+            if less > number:
+                print('yes')
+            else:
+                print('no')
     if int(ans) == number:
         print('correct')
         win = 1
     else:
-        if ans == '<' or '>':
-            if ans == '<':
-                greater = input('greater than?')
-                greater = int(greater)
-                if greater < number:
-                    print('yes')
-                else:
-                    print('no')
-            if ans == '>':
-                less = input('less than?')
-                less = int(less)
-                if less > number:
-                    print('yes')
-                else:
-                    print('no')
-        
-        else:
-            print('incorrect')
-            number_of_guesses += 1
+        print('incorrect')
+        number_of_guesses += 1
 if win == 0:
     print('you lose.')
 else:
     print('you win!')
-            
